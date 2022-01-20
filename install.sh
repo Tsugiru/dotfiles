2b559aab78945d3ffabbdd1e9fed544cf2e77f46
Elie Maamari <eliemaamary17@gmail.com>
2022-01-20 14:55:20 +0100

    remove vim plug

--- install.sh
+++ install.sh
@@ -39,8 +39,6 @@ elif [ $platform == "mac" ]; then
     brew install neovim
     brew install llvm
 fi
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # install tmux
 if [ $platform == "linux" ]; then
